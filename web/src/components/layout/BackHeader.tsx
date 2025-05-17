a582d537d6c7d12246e3bc91eae5ad69f33458a9
hin6150 <hin6150@gmail.com>
2025-05-18 01:19:35 +0900

    Feat : 재사용 가능하게 헤더 분리

---
+++ web/src/components/layout/BackHeader.tsx
@@ -0,0 +1,26 @@
+import LeftIcon from "@/assets/icons/left";
+import { path } from "@/routes/path";
+import type React from "react";
+
+import { Link } from "react-router-dom";
+
+interface BackHeaderProps {
+  title?: string | React.ReactNode;
+  linkPath?: string;
+}
+
+function BackHeader(props: BackHeaderProps) {
+  const { title, linkPath = path.base } = props;
+
+  return (
+    <div className="flex w-full items-center px-4">
+      <Link to={linkPath}>
+        <LeftIcon />
+      </Link>
+
+      {title}
+    </div>
+  );
+}
+
+export default BackHeader;
