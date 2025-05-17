20a1e761e9b8cb313c74b33e65f8029de60c617b
hin6150 <hin6150@gmail.com>
2025-05-18 08:03:03 +0900

    Feat : 스플래시 추가

--- native/app/_layout.tsx
+++ native/app/_layout.tsx
@@ -6,13 +6,24 @@ import { WebView } from 'react-native-webview';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { Platform, SafeAreaView } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { SplashScreen } from 'expo-router';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+
+  useEffect(() => {
+    (async () => {
+      // 리소스 사전 로딩 …
+      await new Promise((r) => setTimeout(r, 1000));
+      await SplashScreen.hideAsync(); // 2) 준비 끝나면 숨김
+    })();
+  }, []);
 
   if (!loaded) {
     // Async font loading only occurs in development.
@@ -24,7 +35,7 @@ export default function RootLayout() {
   Platform.OS === 'android'
     ? 'http://10.0.2.2:5173' 
     : 'http://localhost:5173';
-
+  
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <SafeAreaView style={{ flex: 1 }}>
