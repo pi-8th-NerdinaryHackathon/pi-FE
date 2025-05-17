f8ce7c75cfcdcce0d4617acc6f4f11e1e9a58c56
hin6150 <hin6150@gmail.com>
2025-05-18 01:38:02 +0900

    Feat : 카메라 UI 구현

--- web/src/components/camera/Camera.tsx
+++ web/src/components/camera/Camera.tsx
@@ -2,12 +2,6 @@ function Camera() {
   return (
     <>
       <input
-        className="h-20 w-20 rounded-2xl bg-amber-300"
-        type="file"
-        accept="image/*"
-        capture="environment"
-      />
-      <input
         className="h-20 w-20 rounded-2xl bg-black"
         type="file"
         accept="image/*"
--- web/src/pages/Default.tsx
+++ web/src/pages/Default.tsx
@@ -1,7 +1,23 @@
+import PhotoIcon from "@/assets/icons/photo";
 import MainHeader from "@/components/layout/MainHeader";
 
 function DefaultPage() {
-  return <MainHeader />;
+  return (
+    <div>
+      <MainHeader />
+
+      <div className="fixed bottom-6 left-1/2 flex -translate-x-1/2 gap-2 rounded-[30px] bg-black px-[28px] py-[14px] hover:cursor-pointer">
+        <PhotoIcon />
+        <p className="text-white">사진으로 검색하기</p>
+        <input
+          className="hidden h-20 w-20 rounded-2xl bg-amber-300"
+          type="file"
+          accept="image/*"
+          capture="environment"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default DefaultPage;
